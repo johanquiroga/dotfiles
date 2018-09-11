7fce5395ea4a94f0f9034a5ee2a34c1941462c38
Johan Quiroga <johan.c.quiroga@gmail.com>
2018-09-11 09:36:42 -0500

    Create vscode.js

---
+++ vscode.js
@@ -0,0 +1,63 @@
+{
+    "nativescript.analytics.enabled": false,
+    "atomKeymap.promptV3Features": true,
+    "editor.multiCursorModifier": "ctrlCmd",
+    "editor.formatOnPaste": true,
+    "materialTheme.fixIconsRunning": false,
+    "workbench.iconTheme": "eq-material-theme-icons-darker",
+    "editor.fontSize": 20,
+    "editor.letterSpacing": 0,
+    "editor.fontLigatures": true,
+    // Install Fira Code. @see https://github.com/tonsky/FiraCode
+    "editor.fontFamily": "'Fira Code','Operator Mono', Menlo, Monaco, 'Courier New', monospace",
+    "editor.wordWrap": "on",
+    "editor.renderWhitespace": "none",
+    "editor.tabSize": 2,
+    "editor.cursorStyle": "line",
+    "editor.cursorWidth": 3,
+    "editor.colorDecorators": false,
+    "editor.fontWeight": "400",
+    "editor.scrollBeyondLastLine": false,
+    "files.trimTrailingWhitespace": true,
+    "files.exclude": {
+        "**/.git": true,
+        "**/.svn": true,
+        "**/.hg": true,
+        "**/CVS": true,
+        "**/.DS_Store": true,
+        "**/.next": true,
+        "**/.idea": true
+    },
+    "emmet.includeLanguages": {
+        "javascript": "javascriptreact"
+    },
+    "breadcrumbs.enabled": true,
+    "workbench.activityBar.visible": true,
+    "workbench.sideBar.location": "left",
+    "workbench.startupEditor": "newUntitledFile",
+    "gitlens.codeLens.recentChange.enabled": false,
+    "gitlens.codeLens.authors.enabled": false,
+    "gitlens.hovers.currentLine.over": "line",
+    "gitlens.currentLine.enabled": false,
+    "gitlens.hovers.currentLine.enabled": false,
+    "workbench.colorTheme": "Material Theme Darker High Contrast",
+    "cSpell.language": "en,es",
+    // These are all my auto-save configs
+    "editor.formatOnSave": true,
+    // turn it off for JS
+    "[javascript]": {
+        "editor.formatOnSave": false
+    },
+    "eslint.autoFixOnSave": true,
+    "eslint.alwaysShowStatus": true,
+    "workbench.editor.enablePreviewFromQuickOpen": false,
+    // show snippets before any other auto completion
+    "editor.snippetSuggestions": "top",
+    "window.zoomLevel": 0,
+    "php.suggest.basic": false,
+    "php-cs-fixer.executablePath": "${extensionPath}/php-cs-fixer.phar",
+    "php-cs-fixer.lastDownload": 1535577073578,
+    // Fix path or remove
+    "dart.flutterSdkPath": "/Users/johanquiroga/flutter",
+    "workbench.statusBar.visible": true,
+}
